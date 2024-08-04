99d417ad196a2917465b0eec7122fb92a73825dd
Abel Aghorighor <52415319+aaghorighor@users.noreply.github.com>
2024-08-04 18:49:10 +0100

    first commit

--- src/hooks/useBluetoothPermissions.tsx
+++ src/hooks/useBluetoothPermissions.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable no-empty */
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-undef */
 /* eslint-disable prettier/prettier */
 import { useState, useEffect } from 'react';
@@ -13,7 +15,7 @@ const useBluetoothPermission = () => {
                 const state = await BluetoothManager.getState();               
                 setIsEnabled(state === 'PoweredOn');
             } catch (error) {
-                console.error('Error fetching Bluetooth state:', error);
+              
             }
         };
         checkBluetoothState();
@@ -22,12 +24,15 @@ const useBluetoothPermission = () => {
 
     const requestPermissions = async () => {
         try {
-            const result = await BluetoothManager.requestToEnable();
-            if (result) {
+            const state = await BluetoothManager.getState();  
+            if (state === 'PoweredOff')  {               
+             await BluetoothManager.enable();               
+            } else if (state === 'PoweredOn') {
                 setIsEnabled(true);
-            } 
+            }
+           
         } catch (error) {
-            console.error('Error requesting Bluetooth enable:', error);          
+                   
         }
     };
 
--- src/hooks/useBluetoothPrinter.jsx
+++ src/hooks/useBluetoothPrinter.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BluetoothManager } from 'tp-react-native-bluetooth-printer';
 import { printReceipt, receiptTestData } from '../utils/printReceipt';
-import useBluetoothPermissions from './useBluetoothPermissions';
+import useCheckAndRequestBluetoothPermission from './useCheckAndRequestBluetoothPermission';
 
 const useBluetoothPrinter = () => { 
     const [devices, setDevices] = useState([]);
@@ -13,12 +13,12 @@ const useBluetoothPrinter = () => {
     const [selectedPrinter, setSelectedPrinter] = useState(null)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false)
-    const { requestPermissions, isEnabled } = useBluetoothPermissions()
+    const { isEnabled } = useCheckAndRequestBluetoothPermission()
 
     useEffect(() => {
-        !isEnabled && requestPermissions();
         loadSelectedPrinter();
-    }, [isEnabled]);
+    }, []);
+
    
     const enableBluetooth = () => {
         BluetoothManager.enableBluetooth().then(devices => {
